d13bb2a7e60db3fe3b84b0f04763d7a136ea2ca7
claridiva2000 <claridiva2000@gmail.com>
2019-01-30 22:58:36 -0600

    half done

--- js/index.js
+++ js/index.js
@@ -37,6 +37,67 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
+//body
+const body= document.getElementsByTagName('body');
+
+//header
+const header = document.getElementsByTagName('header');
+
+//logo-img
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//nav
+const navitems = document.getElementsByTagName("nav");
+console.log(navitems);
+//all a-tags
+const links = document.querySelectorAll('a');
+console.log(links);
+//indv. a-tags
+const navlink1 = document.links[0];
+navlink1.textContent= siteContent["nav"]["nav-item-1"];
+
+const navlink2 = document.links[1];
+navlink2.textContent= siteContent["nav"]["nav-item-2"];
+
+const navlink3 = document.links[2];
+navlink3.textContent= siteContent["nav"]["nav-item-3"];
+
+const navlink4 = document.links[3];
+navlink4.textContent= siteContent["nav"]["nav-item-4"];
+
+const navlink5 = document.links[4];
+navlink5.textContent= siteContent["nav"]["nav-item-5"];
+
+const navlink6 = document.links[5];
+navlink6.textContent= siteContent["nav"]["nav-item-6"];
+
+//circle-img
+const circleimg = document.getElementById('cta-img');
+circleimg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+//H1
+const ctaText = document.querySelector("h1");
+console.log(ctaText);
+ctaText.textContent= siteContent["cta"]["h1"];
+
+//BUTTON
+const button = document.querySelector('button');
+console.log(button);
+button.textContent= siteContent["cta"]["button"];
+
+//middle.img
+const middleImg= document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+console.log(middleImg);
+
+
+
+
+
+
+
+
+// Example: Update the img src for the logo
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
