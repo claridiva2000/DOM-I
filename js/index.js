a1ea4558bfc728646b9586488dcdde10fe0c57ae
claridiva2000 <claridiva2000@gmail.com>
2019-01-31 02:07:37 -0600

    done

--- js/index.js
+++ js/index.js
@@ -40,36 +40,31 @@ const siteContent = {
 //body
 const body= document.getElementsByTagName('body');
 
-//header
-const header = document.getElementsByTagName('header');
 
 //logo-img
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-//nav
-const navitems = document.getElementsByTagName("nav");
-console.log(navitems);
 //all a-tags
 const links = document.querySelectorAll('a');
 console.log(links);
 //indv. a-tags
-const navlink1 = document.links[0];
+const navlink1 = links[0];
 navlink1.textContent= siteContent["nav"]["nav-item-1"];
 
-const navlink2 = document.links[1];
+const navlink2 = links[1];
 navlink2.textContent= siteContent["nav"]["nav-item-2"];
 
-const navlink3 = document.links[2];
+const navlink3 = links[2];
 navlink3.textContent= siteContent["nav"]["nav-item-3"];
 
-const navlink4 = document.links[3];
+const navlink4 = links[3];
 navlink4.textContent= siteContent["nav"]["nav-item-4"];
 
-const navlink5 = document.links[4];
+const navlink5 = links[4];
 navlink5.textContent= siteContent["nav"]["nav-item-5"];
 
-const navlink6 = document.links[5];
+const navlink6 = links[5];
 navlink6.textContent= siteContent["nav"]["nav-item-6"];
 
 //circle-img
@@ -91,12 +86,57 @@ const middleImg= document.getElementById("middle-img");
 middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 console.log(middleImg);
 
+//all h4's
+const hs = document.querySelectorAll('h4');
+console.log(hs);
+const h41 = hs[0];
+h41.textContent= siteContent['main-content']['features-h4'];
 
+const h42 = hs[1];
+h42.textContent= siteContent["main-content"]["about-h4"];
 
+const h43 = hs[2];
+h43.textContent= siteContent["main-content"]["services-h4"];
 
+const h44 = hs[3];
+h44.textContent= siteContent["main-content"]["product-h4"];
 
+const h45 = hs[4];
+h45.textContent= siteContent["main-content"]["vision-h4"];
 
+const h46 = hs[5];
+h46.textContent= siteContent["contact"]["contact-h4"];
 
+// all p's
+const ps= document.querySelectorAll('p');
+console.log(ps);
+
+const ps1 = ps[0];
+ps1.textContent= siteContent["main-content"]["features-content"]
+
+const ps2 = ps[1];
+ps2.textContent= siteContent["main-content"]["about-content"]
+
+const ps3 = ps[2];
+ps3.textContent= siteContent["main-content"]["services-content"]
+
+const ps4 = ps[3];
+ps4.textContent= siteContent["main-content"][""]
+
+const ps5 = ps[4];
+ps5.textContent= siteContent["main-content"]["features-content"]
+
+const ps6 = ps[5];
+ps6.textContent= siteContent["contact"]["address"];
+
+const ps7 = ps[6];
+ps7.textContent= siteContent["contact"]["phone"];
+
+const ps8 = ps[7];
+ps8.textContent= siteContent["contact"]["email"];
+
+const ps9 = ps[8];
+ps9.textContent= siteContent["footer"]["copyright"];
 
 // Example: Update the img src for the logo
 // let logo = document.getElementById("logo-img");
